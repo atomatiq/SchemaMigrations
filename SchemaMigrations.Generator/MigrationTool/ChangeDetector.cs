0e4d0a138690b70cd3d807eb271d12fdec69154c
SergeyNefyodov <nefyodov.sergei@gmail.com>
2024-10-15 10:53:49 +0200

    Add raw strings

--- SchemaMigrations.Generator/MigrationTool/ChangeDetector.cs
+++ SchemaMigrations.Generator/MigrationTool/ChangeDetector.cs
@@ -18,11 +18,11 @@ public static class ChangeDetector
                     var genericTypeName = type.GetGenericTypeDefinition().Name;
                     var genericArguments = string.Join(", ", type.GetGenericArguments().Select(t => t.Name));
                     var fullTypeName = $"{genericTypeName.Substring(0, genericTypeName.IndexOf('`'))}<{genericArguments}>";
-                    changes.Add( $"AddColumn(\"{schemaName}\", \"{pair.Key}\", typeof({fullTypeName}))");
+                    changes.Add( $"""AddColumn("{schemaName}", "{pair.Key}", typeof({fullTypeName}))""");
                 }
                 else
                 {
-                    changes.Add( $"AddColumn(\"{schemaName}\", \"{pair.Key}\", typeof({type.Name}))");
+                    changes.Add( $"""AddColumn("{schemaName}", "{pair.Key}", typeof({type.Name}))""");
                 }
             }
             else if (value != pair.Value)
